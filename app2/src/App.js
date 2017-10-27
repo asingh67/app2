3cec1f63096efbacd26651f602a9383c8b0e074f
asingh67 <32086062+asingh67@users.noreply.github.com>
2017-10-27 22:16:58 +0530

    Add material-ui Appbar to the App

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import AppBar from "material-ui/AppBar"
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
+        <AppBar
+        title="mAppBar"
+        iconClassNameRight="muidocs-icon-navigation-expand-more" />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
